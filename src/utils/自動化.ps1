9e28319ea6e5a6e8fc420f74da78d728d7662229
yttcdfm <ytt.akbakb84@gmail.com>
2020-11-05 01:38:31 +0900

    汎用自動化スクリプト

---
+++ src/utils/自動化.ps1
@@ -0,0 +1,11 @@
+﻿cd "C:\Program Files\MacroCreator"
+.\MacroCreator.exe "C:\Users\yttak\Desktop\hogehoge\hogehoge.pmc"
+
+Add-Type -AssemblyName System.Windows.Forms
+Start-Sleep -m 500
+
+[System.Windows.Forms.SendKeys]::SendWait("%{TAB}")
+Start-Sleep -m 10000
+[System.Windows.Forms.SendKeys]::SendWait("^+~")
+
+Start-Sleep -m 60
