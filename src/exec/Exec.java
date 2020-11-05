630a32fcc55a4e2ff2ccb014ac994480d5b7ed47
yttcdfm <ytt.akbakb84@gmail.com>
2020-11-05 21:57:02 +0900

    testswa

--- src/exec/Exec.java
+++ src/exec/Exec.java
@@ -8,7 +8,7 @@ public class Exec {
 	public static void main(String[] args) {
 		ThreadTest threadTest = new ThreadTest();
 		threadTest.start();
-
+System.out.println("‚Š‚‹‚Œ‚‡‚¶‚áG‚Œ‚Š‚ª‚‹G‚Š");
 		System.out.println("fuga69");
 	}
 }
