a58bc5a324e5d2146b779fee1ee516b6bf48ef7a
yttcdfm <ytt.akbakb84@gmail.com>
2020-11-05 21:29:02 +0900

    test

---
+++ src/exec/Exec.java
@@ -0,0 +1,15 @@
+package exec;
+
+import utils.TcpServer;
+import utils.ThreadTest;
+
+public class Exec {
+	
+	public static void main(String[] args) {
+		ThreadTest threadTest = new ThreadTest();
+		threadTest.start();
+		System.out.println("hogehoge1");
+		System.out.println("hogehoge2");
+		System.out.println("hogehoge3");
+	}
+}
