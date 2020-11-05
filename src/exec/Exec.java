b6342acceef0673fb058ee48b7d256dd24b152b0
yttcdfm <ytt.akbakb84@gmail.com>
2020-11-05 21:55:01 +0900

    test

--- src/exec/Exec.java
+++ src/exec/Exec.java
@@ -1,5 +1,7 @@
 package exec;
 
+import com.sun.accessibility.internal.resources.accessibility_ja;
+
 import utils.TcpServer;
 import utils.ThreadTest;
 
@@ -8,7 +10,7 @@ public class Exec {
 	public static void main(String[] args) {
 		ThreadTest threadTest = new ThreadTest();
 		threadTest.start();
-
 		System.out.println("fuga69");
+		System.out.println("hgeoiajiopajiopa");
 	}
 }
