1719159f617ac022d1856172bfc6ca6333f65d04
yttcdfm <ytt.akbakb84@gmail.com>
2020-11-09 23:53:49 +0900

    続き

--- src/utils/TcpServer.java
+++ src/utils/TcpServer.java
@@ -5,22 +5,59 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 public class TcpServer {
 	
-	private Integer port;
 	private ServerSocket serverSocket;
+	private Integer port;
 	private BufferedReader bufferedReader;
 	private DataOutputStream output;
+	private Boolean isEnd;
 	
 	public TcpServer(Integer port) {
 		this.port = port;
 	}
 	
+	private String getMacAddress(Inet4Address inetAddress) {
+		String macAddress = "";
+		
+		try {
+			NetworkInterface networkInterface = NetworkInterface.getByInetAddress(inetAddress);
+			byte[] str = networkInterface.getHardwareAddress();
+			
+			if(str.length == 6) {
+				for(int i = 0; i < str.length; i++) {
+					macAddress += String.format("%02x", str[i]);
+					macAddress += i <= 4 ? ":" : "";
+				}
+			}else {
+				macAddress = "xx:xx:xx:xx:xx:xx";
+			}
+			
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+
+		return macAddress;
+	}
+	
 	public void exec(){		
 		try {
+			System.out.println("マシン情報");
+			Inet4Address inetAddress = (Inet4Address) Inet4Address.getLocalHost();
+			System.out.println("  * ホスト名\t" + inetAddress.getCanonicalHostName());
+			System.out.println("  * IPアドレス\t" + inetAddress.getHostAddress());
+			String macAddress = getMacAddress(inetAddress);
+			System.out.println("  * MACアドレス\t" + macAddress);
+			
 			serverSocket = new ServerSocket(port);
 			System.out.println("TCPサーバ接続待ち");
 			Socket socket = serverSocket.accept();
@@ -28,11 +65,8 @@ public class TcpServer {
 			
 			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			output = new DataOutputStream(socket.getOutputStream());
-			String moji = bufferedReader.readLine();	//byte取り出したいときはDataInputStream
-			
-			System.out.println("　入力:" + moji);
-			output.writeChars(moji + 2);
-			System.out.println("　出力:" + moji + 2);
+
+			execProccess();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -46,5 +80,26 @@ public class TcpServer {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	public void execProccess() throws IOException {
+		String moji = bufferedReader.readLine();	//byte取り出したいときはDataInputStream
+		System.out.println("　入力:" + moji);
+		output.writeChars(moji + 2);
+		System.out.println("　出力:" + moji + 2);
+		setIsEnd(true);
+	}
+	
+	public void setIsEnd(Boolean isEnd) {
+		this.isEnd = isEnd;
+	}
+	
+	public Boolean getIsEnd() {
+		return this.isEnd;
+	}
+	
+	public static void main(String[] args) {
+		TcpServer tcpServer = new TcpServer(10001);
+		tcpServer.exec();
 	}
 }
